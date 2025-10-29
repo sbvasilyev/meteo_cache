78d9407c56ef37c450210c8f47ce341f0d9b2b4c
Sergey Vasilyev <sbvasilyev@gmail.com>
2025-10-29 15:39:45 +0300

    added middleware for errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,7 @@
+use serde::{Serialize, Serializer};
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Serialize)]
 pub enum MeteoError {
     #[error("[weather_service] error: `{0}`")]
     WeatherServiceError(String),
@@ -9,8 +10,24 @@ pub enum MeteoError {
     RequestError(String),
 
     #[error("[tarantool] error: ")]
-    TarantoolError(#[from] tarantool::error::Error),
+    TarantoolError(
+        #[from]
+        #[serde(serialize_with = "as_display")]
+        tarantool::error::Error,
+    ),
 
     #[error("[serde_json] error: ")]
-    SerdeError(#[from] serde_json::Error),
+    SerdeError(
+        #[from]
+        #[serde(serialize_with = "as_display")]
+        serde_json::Error,
+    ),
+}
+
+fn as_display<T, S>(value: &T, ser: S) -> Result<S::Ok, S::Error>
+where
+    T: std::fmt::Display,
+    S: Serializer,
+{
+    ser.serialize_str(&value.to_string())
 }
--- src/router.rs
+++ src/router.rs
@@ -1,16 +1,23 @@
 use shors::transport::{
     Context,
-    http::{Request, route::Builder, server::Server},
+    http::{
+        Request, Response,
+        route::{Builder, Handler},
+        server::Server,
+    },
 };
 
-use crate::om_service::OpenMeteoService;
+use crate::{errors::MeteoError, om_service::OpenMeteoService};
 
 mod handlers;
 
 #[tarantool::proc]
 pub fn http_init() -> tarantool::Result<()> {
     let server = Server::new();
-    let route_group = Builder::new().with_path("/rs").group();
+    let route_group = Builder::new()
+        .with_path("/rs")
+        .with_middleware(error_middleware)
+        .group();
 
     let ping = route_group
         .builder()
@@ -30,4 +37,18 @@ pub fn http_init() -> tarantool::Result<()> {
     server.register(Box::new(forecast));
 
     Ok(())
+}
+
+fn error_middleware(handler: Handler<MeteoError>) -> Handler<MeteoError> {
+    Handler(Box::new(move |ctx, req| match handler(ctx, req) {
+        Ok(resp) => Ok(resp),
+        Err(err) => Ok(Response {
+            status: 400,
+            body: serde_json::to_vec(&err)?,
+            headers: std::collections::HashMap::from([(
+                "content-type".to_string(),
+                "application/json; charset=utf8".to_string(),
+            )]),
+        }),
+    }))
 }
